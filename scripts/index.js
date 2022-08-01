472a069c7dc3f5400bf685274f3890e30eedad1c
terracotta <begaiym.akylova@gmail.com>
2022-08-01 18:09:23 +0600

    Add faves and repos features

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 import "./customHeader.js";
 import { createCardUser } from "./createCard.js";
+import { getRepos } from "./getRepos.js";
+import { addToFaves } from "./faves.js";
 
 const userName = document.querySelector("#username");
 const sort = document.querySelector("#sort");
@@ -20,6 +22,10 @@ if (userName) {
   prevButton.addEventListener("click", prevPage);
 }
 
+export const faves = localStorage.getItem("faves")
+  ? JSON.parse(localStorage.getItem("faves"))
+  : localStorage.setItem("faves", JSON.stringify({}));
+
 // ================== PAGINATION ==================
 
 function nextPage() {
@@ -39,15 +45,13 @@ function prevPage() {
 // ================== SEARCH USERS ==================
 
 async function getUsers(e) {
-  clear();
-  let queryString = `${url}users?q=${userName.value}&per_page=${perPage.value}&page=${currentPageValue}&s=${sort.value}&order=desc`;
-  console.log(queryString);
+  clear(results);
+  let queryString = `${url}users?q=${userName.value}&per_page=${perPage.value}&page=${currentPageValue}&sort=${sort.value}&order=desc`;
   const response = await fetch(queryString);
   if (response.ok) {
     const result = await response.json();
-		let totalItems = result.total_count > 999 ? 999 : result.total_count
+    let totalItems = result.total_count > 999 ? 999 : result.total_count;
     maxPages = Math.ceil(totalItems / perPage.value);
-    console.log(maxPages);
     result.items.forEach((user) => {
       const card = createCardUser(
         user.id,
@@ -55,27 +59,11 @@ async function getUsers(e) {
         user.login,
         user.html_url,
         getRepos,
-        someFunc
+        addToFaves
       );
       results.appendChild(card);
     });
   }
-}
-
-// ================== SEARCH REPOS ==================
-
-async function getRepos(e) {
-  location.href = "./repos.html";
-  clear();
-  const id = e.target.textContent;
-  // const queryString = `${url}repositories?q=id=`
-  ("https://api.github.com/search/repositories?q=stars:150000..300000");
-  const response = await fetch(url);
-  const result = await response.json();
-}
-
-function someFunc() {
-  console.log("isfave");
 }
 
 // ================== DELAY FUNCTION ==================
@@ -98,6 +86,6 @@ function debounce(func, wait, immediate) {
 
 // ================== CLEARING RESULTS ==================
 
-function clear() {
-  while (results.firstChild) results.removeChild(results.firstChild);
+export function clear(container) {
+  while (container.firstChild) container.removeChild(container.firstChild);
 }
